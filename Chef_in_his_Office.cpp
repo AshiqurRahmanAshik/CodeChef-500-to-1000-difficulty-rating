527a7ac3e1a90096a7fb47d1d3e70645296f012d
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-12-02 06:57:55 +0600

    Chef in his office

---
+++ Chef_in_his_Office.cpp
@@ -0,0 +1,20 @@
+/*
+Problem Link: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/OFFICE
+ Date: 02/12/25
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int x, y;
+        cin >> x >> y;
+        cout << (4 * x) + y << endl;
+    }
+    return 0;
+}
