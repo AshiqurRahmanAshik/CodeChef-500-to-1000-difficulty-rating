b9dc48c49a7cecd59274f33d98acf5b153c714b0
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-12-04 12:08:52 +0600

    Sugercane and Business

---
+++ Sugarcane_Juice_Business.cpp
@@ -0,0 +1,18 @@
+/*
+Problem Link: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/SUGARCANE
+ Date: 04/12/205
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        long long n;
+        cin >> n;
+        cout << 15 * n << endl;
+    }
+    return 0;
+}
