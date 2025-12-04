843a9d53c3e248da81c3c2a1cd2b04a800b29216
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-12-04 12:12:29 +0600

    Count the notebooks

---
+++ Count_the_Notebooks.cpp
@@ -0,0 +1,20 @@
+/*
+Problem Link: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/NOTEBOOK
+ Date: 04/12/25
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        cout << 10 * n << endl;
+    }
+    return 0;
+}
