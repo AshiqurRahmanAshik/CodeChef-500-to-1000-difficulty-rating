3336bf5655317136253cdbcd9aa400d6bc2cc28a
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-12-05 16:03:52 +0600

    Watching movies at 2

---
+++ Watching_Movies_at_2_x.cpp
@@ -0,0 +1,16 @@
+/*
+Problem Link: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/MOVIE2X
+ Date:  05/12/25
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int x, y;
+    cin >> x >> y;
+    cout << (y / 2) + (x - y) << endl;
+    return 0;
+}
