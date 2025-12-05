0fa7385e99c4d4ca710a793f7eec83242e06a4ad
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-12-05 14:34:34 +0600

    Minimum Cars Required

---
+++ Minimum_Cars_required.cpp
@@ -0,0 +1,20 @@
+/*
+Problem Link: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/MINCARS
+ Date:  05/12/25
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        cout << (n + 3) / 4 << endl;
+    }
+    return 0;
+}
