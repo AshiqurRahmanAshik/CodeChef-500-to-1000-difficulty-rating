2f50a08183b95d7338a6e9765b152a92c9e65b24
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2026-01-05 00:25:48 +0600

    Too many items

---
+++ Too_many_items.cpp
@@ -0,0 +1,20 @@
+/*
+Problem Link: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/POLYBAGS
+ Date:  05/01/26
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        cout << (n + 9) / 10 << endl;
+    }
+    return 0;
+}
